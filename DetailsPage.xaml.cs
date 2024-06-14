19b8667909cc0565f6e580752e45537b4a0199df
radu1351 <radu1351@yahoo.com>
2024-06-14 13:21:36 +0300

    Added list of houses.

---
+++ DetailsPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Imobiliare;
+
+public partial class DetailsPage : ContentPage
+{
+	public DetailsPage()
+	{
+		InitializeComponent();
+	}
+
+	public DetailsPage(RealEstate realEstate)
+	{
+        InitializeComponent();
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,46 @@
-﻿namespace Imobiliare
+﻿using System.Collections.ObjectModel;
+
+namespace Imobiliare
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public ObservableCollection<RealEstate> RealEstateItems { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+            RealEstateItems = new ObservableCollection<RealEstate>
+            {
+                new RealEstate { ImageUrl = "house1.jpg", Name = "Casa", Price = "$200,000" },
+                new RealEstate { ImageUrl = "house1.jpg", Name = "Casa", Price = "$250,000" },
+                // Add more real estate items here
+            };
+            BindingContext = this;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnAddRealEstateClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            // Add logic to add a new real estate item
+            RealEstateItems.Add(new RealEstate { ImageUrl = "house2.jpeg", Name = "Casa", Price = "$300,000" });
         }
+
+        private async void OnItemTapped(object sender, EventArgs e)
+        {
+            var frame = sender as Frame;
+            var realEstate = frame.BindingContext as RealEstate;
+            if (realEstate != null)
+            {
+                await Navigation.PushAsync(new DetailsPage(realEstate));
+            }
+        }
+    }
+
+   
+    public class RealEstate
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
     }
 
 }
